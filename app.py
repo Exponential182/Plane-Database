a1938f6c2dcb835415cd6b9b26c94fb167d9ad5f
Exponential182 <147586495+Exponential182@users.noreply.github.com>
2024-04-11 12:21:11 +1200

    Added Basic code to draw information

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+import sqlite3
 
-    
+database = 'fighters.db'
+db = sqlite3.connect(database)
+cursor = db.cursor()
+query = 'SELECT aircraft, climbrate FROM fighters WHERE climbrate > 100'
+cursor.execute(query)
+results = cursor.fetchall()
+print(results)
+
+
+db.close()
+
