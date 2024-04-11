ce5c7d622bdfd4965068bdc9c2a8e7b75e905800
Exponential182 <147586495+Exponential182@users.noreply.github.com>
2024-04-11 12:45:46 +1200

    Improved the Return of Data and some syntax

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
+''' Coded By Matthew Thomas, designed to draw information from a databse on military fighter jets'''
+#Imports
 import sqlite3
 
-database = 'fighters.db'
-db = sqlite3.connect(database)
-cursor = db.cursor()
-query = 'SELECT aircraft, climbrate FROM fighters WHERE climbrate > 100'
-cursor.execute(query)
-results = cursor.fetchall()
-print(results)
+#Constants and Variables
+DATABASE = 'fighters.db'
 
 
-db.close()
+#Functions
+def print_all_aircraft():
+    '''Function to print all aircraft in the database nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    query = 'SELECT * FROM fighters;'
+    cursor.execute(query)
+    results = cursor.fetchall()
+    #loop through all the results
+    for fighter in results:
+        print(f"{fighter[1]:<40}{fighter[2]:<7}{fighter[3]:<7}{fighter[4]:<7}{fighter[5]:<7}{fighter[6]:<7}")
+    #loop complete
+    db.close()
+
+
+
+#main
+print_all_aircraft()
+
+
+
 
